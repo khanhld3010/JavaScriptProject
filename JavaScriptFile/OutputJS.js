410aaab38c8539a9989836a75418e6dd3f2336ff
khanhld3010 <luuduckhanh3010@gmail.com>
2026-03-09 09:23:09 +0700

    Onclick event

---
+++ JavaScriptFile/OutputJS.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    alert("Xin chào")
+}
+
+function hideMessage() {
+    document.getElementById("hiddenMessage").innerHTML = "xin chào";
+}
+
+function hideMessage1() {
+    document.write("Xin chào")
+}
+
+function consoleLog() {
+    console.log("Xin Chào")
+}
