1812a296f5e42395c6f1639bafbe7a15e6ad3684
khanhld3010 <luuduckhanh3010@gmail.com>
2026-03-09 11:21:15 +0700

    Variable Example

---
+++ JavaScriptFile/Variable.js
@@ -0,0 +1,37 @@
+//bài 1
+
+// var i = 10;
+// var f = 20.5;
+// var b = true
+// var c = "Hà Nội";
+//
+// document.write('i = ' + i)
+// document.write('<br/>')
+// document.write('f = ' + f)
+// document.write('<br/>')
+// document.write('b = ' + b)
+// document.write('<br/>')
+// document.write('s = ' + s)
+
+
+//Bài 2
+// document.write('<br/>')
+// document.write('<h1> Area </h1>')
+// let width = 20;
+// let height = 10;
+// let area = width*height;
+// document.write("Width = "+width);
+// document.write('<br/>')
+// document.write("Height = "+height);
+// document.write('<br/>')
+// document.write("Area = "+area);
+
+//Bài 3
+
+firstNumber = prompt("What is the first number?");
+secondNumber = prompt("What is the second number?");
+if (firstNumber % secondNumber === 0) {
+    alert("A là bội số của B");
+}else {
+    alert("A không phải là bội số của B");
+}
