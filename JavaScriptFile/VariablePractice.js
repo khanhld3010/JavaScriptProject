f870e48c9eba1efaa4ba4e92d0aba9b11cb562fc
khanhld3010 <luuduckhanh3010@gmail.com>
2026-03-09 11:49:44 +0700

    Variable Example

---
+++ JavaScriptFile/VariablePractice.js
@@ -0,0 +1,6 @@
+inputWidth = prompt("Input Width:");
+inputHeight = prompt("Input Height:");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("Area: " + area);
